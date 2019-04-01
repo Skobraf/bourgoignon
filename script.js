308cf07e9299d08bceed78fad57274add19d5ef4
skobraf <skobraf@gmail.com>
2019-04-01 16:31:42 +0100

    added JavaScript to switch to fixed navigatin

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  
+    $(window).scroll(function () {
+      console.log($(window).scrollTop())
+      if ($(window).scrollTop() > 144) {
+        console.log('buttom');
+        $('#nav').addClass('on-scroll-position');
+        $('#menu').addClass('on-scroll-switch');
+        $('#button').addClass('on-scroll-button');
+      }
+      if ($(window).scrollTop() < 145) {
+        console.log('top')
+        $('#nav').removeClass('on-scroll-position');
+        $('#menu').removeClass('on-scroll-switch');
+        $('#button').removeClass('on-scroll-button');
+      }
+    });
+  });

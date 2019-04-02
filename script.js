f90b27ec7ff53d2c563a8c4e93ab258bec51ed71
skobraf <skobraf@gmail.com>
2019-04-02 14:55:32 +0100

    update script with better result

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
   
     $(window).scroll(function () {
       console.log($(window).scrollTop())
-      if ($(window).scrollTop() > 144) {
+      if ($(window).scrollTop() > 0) {
         console.log('buttom');
         $('#nav').addClass('on-scroll-position');
         $('#menu').addClass('on-scroll-switch');
         $('#button').addClass('on-scroll-button');
       }
-      if ($(window).scrollTop() < 145) {
+      if ($(window).scrollTop() < 1) {
         console.log('top')
         $('#nav').removeClass('on-scroll-position');
         $('#menu').removeClass('on-scroll-switch');
